88d8bb9a899622a0af43fe00f0b71148c5393a5f
Pratapworld <pratapkumarworld@gmail.com>
2026-06-12 23:05:08 +0530

    Add table/CrackPass binaries, sources and dSYMs
    Add two C programs (Table2.c, table.c) and their compiled binaries (Table2, table), along with dSYM debug bundles (Info.plist, DWARF files and aarch64 relocation YAMLs) for each. Also add CrackPass dSYM resources and a .DS_Store. Table2 prints even numbers (2..20), table prints the multiplication table for 2; debug symbols are included for local debugging and symbolication.

---
+++ table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a = 2;
+    int b = 1;
+    while (b <= 10)
+        {
+        int c = a * b;
+        printf("%d\n",c);
+        b++;
+        }
+
+    return 0;
+}
